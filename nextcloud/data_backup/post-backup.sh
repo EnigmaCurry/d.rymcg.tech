6208a41261bc87cc7109f0acfaa094b184081224
EnigmaCurry <ryan@enigmacurry.com>
2022-01-24 18:19:48 -0800

    Nextcloud `make enable_maintenance` and `make disable_maintenance`

--- nextcloud/data_backup/post-backup.sh
+++ nextcloud/data_backup/post-backup.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-CONFIG=/data/config/config.php
+/hooks/maintenance.sh off
 
-# https://docs.nextcloud.com/server/latest/admin_manual/maintenance/backup.html
-sed -i "s/'maintenance' => true,/'maintenance' => false,/" ${CONFIG}
-echo "Disabled maintenance mode"
