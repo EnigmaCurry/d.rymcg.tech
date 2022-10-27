35b569f6ca0674ddd72335181db74845023d7651
EnigmaCurry <ryan@enigmacurry.com>
2022-10-27 11:04:07 -0700

    docker_vm: fix VMNAME variables

--- _docker_vm/systemd_install.sh
+++ _docker_vm/systemd_install.sh
@@ -6,7 +6,7 @@ SERVICE=${HOME}/.config/systemd/user/docker-vm.service
 SCRIPT_ROOT=$(dirname $(realpath ${BASH_SOURCE}))
 HOSTFWD_HOST=${HOSTFWD_HOST:-127.0.0.1}
 EXTRA_PORTS=${EXTRA_PORTS:-8000:80,8443:443,5432:5432}
-VM_NAME=${VM_NAME:-docker-vm}
+VMNAME=${VMNAME:-docker-vm}
 
 if loginctl show-user ${USER} | grep "Linger=no"; then
 	  echo "User account does not allow systemd Linger."
@@ -20,8 +20,8 @@ cat <<EOF > ${SERVICE}
 Description=Docker Virtual Machine (${SCRIPT_ROOT})
 
 [Service]
-ExecStart=make -C ${SCRIPT_ROOT} HOSTFWD_HOST="${HOSTFWD_HOST}" EXTRA_PORTS="${EXTRA_PORTS}" VM_NAME="${VM_NAME}"
-ExecStop=make -C ${SCRIPT_ROOT} down
+ExecStart=make -C ${SCRIPT_ROOT} HOSTFWD_HOST="${HOSTFWD_HOST}" EXTRA_PORTS="${EXTRA_PORTS}" VMNAME="${VMNAME}"
+ExecStop=make -C ${SCRIPT_ROOT} down VMNAME="${VMNAME}"
 
 [Install]
 WantedBy=default.target
