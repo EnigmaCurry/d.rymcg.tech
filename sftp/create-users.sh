730ad9f00d2ee635bbf24ae64a9ed5197ff1995d
EnigmaCurry <ryan@enigmacurry.com>
2022-11-15 12:21:26 -0800

    sftp

--- sftp/create-users.sh
+++ sftp/create-users.sh
@@ -6,7 +6,7 @@ IFS=',' read -ra users <<< "$SFTP_USERS"
 for user_entry in "${users[@]}"; do
     parts=(${user_entry//:/ }); user=${parts[0]}; uid=${parts[@]:1};
     addgroup --gid "${uid}" "${user}"
-    adduser --home "/data/${user}" --disabled-password "${user}" --gecos GECOS --uid "${uid}" --gid "${uid}"
-    chown root:root "/data/${user}"
+    adduser --no-create-home --home "/${user}" --disabled-password "${user}" --gecos GECOS --uid "${uid}" --gid "${uid}"
+    mkdir -p "/data/${user}/${user}" && chmod o-rwx "/data/${user}/${user}" && chown "${user}:${user}" "/data/${user}/${user}"
     echo "Created user: ${user}"
 done
