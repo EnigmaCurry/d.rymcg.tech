561609efa44ea81e87824a289dee1885ef7fda46
EnigmaCurry <ryan@enigmacurry.com>
2022-11-26 13:37:46 -0800

    tiddlywiki watcher better threading

--- tiddlywiki-nodejs/watcher/watcher.py
+++ tiddlywiki-nodejs/watcher/watcher.py
@@ -19,15 +19,30 @@ logging.basicConfig()
 logger = logging.getLogger("watcher")
 logger.setLevel(level=logging.INFO)
 
+SLEEP = 5
+
 task_re = re.compile("syncer-server-filesystem: Dispatching '(\w+)' task: (.+)")
 image_types = ['jpg', 'png', 'gif']
-task_queue = queue.Queue()
 canonical_uri_prefix = os.environ.get("TIDDLYWIKI_NODEJS_EXTERNAL_CANONICAL_URI","")
 if canonical_uri_prefix == "":
     canonical_uri_prefix = f"https://{os.environ['TIDDLYWIKI_HOST']}/s3-proxy"
 
-sync_lock = threading.Lock()
-render_lock = threading.Lock()
+class RenderSchedule():
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.scheduled = False
+    def schedule(self, value=True):
+        with self.lock:
+            self.scheduled = value
+    def get_work(self, sleep=0.2):
+        while True:
+            if self.scheduled:
+                self.schedule(False)
+                return True
+            time.sleep(sleep)
+
+task_queue = queue.Queue()
+render_schedule = RenderSchedule()
 
 @retry(wait=wait_fixed(1), stop=stop_after_delay(60))
 def wait_for_file(path):
@@ -51,16 +66,11 @@ endpoint = {endpoint}
     with open(os.path.join(parent,"rclone.conf"), "w") as f:
         f.write(content)
 
-def rclone_sync_s3(remote="s3"):
-    command = f"rclone sync /tiddlywiki {remote}:{os.environ['S3_BUCKET']}/tiddlywiki"
-    with sync_lock:
-        process = rclone_utils.run_cmd(command)
-    if process.returncode == 0:
-        logger.info("Successfully synced to S3")
-    else:
-        logger.error(f"S3 sync failed! \n{process.stderr}")
-
 def publish_static_wiki():
+    print("Queueing render ...")
+    render_schedule.schedule()
+
+def render_worker():
     def render_static_wiki(tiddler_filter="[!is[system]]", template="$:/core/templates/static.tiddler.html"):
         command = f"tiddlywiki --build"
         process = rclone_utils.run_cmd(command)
@@ -75,7 +85,7 @@ def publish_static_wiki():
             data = json.loads(soup.find_all("script",class_="tiddlywiki-tiddler-store")[0].text)
             tiddlers = {x.get("title",""):x for x in data}
         return tiddlers
-    def edit_html_tiddlers(default_tiddlers="[tag[Journal]!sort[]limit[10]]",
+    def edit_html_tiddlers(default_tiddlers=os.environ['TIDDLYWIKI_PUBLIC_DEFAULT_TIDDLERS'],
                            wiki_html="/tiddlywiki/output/index.html",
                            allowed_tags=["Journal"]):
         tiddlers = get_html_tiddlers(wiki_html)
@@ -94,16 +104,28 @@ def publish_static_wiki():
         with open(tmp_file, "w", encoding="utf-8") as f:
             f.write(str(soup))
         return tmp_file
-    with render_lock:
+    def rclone_sync_s3(remote="s3"):
+        command = f"rclone sync /tiddlywiki {remote}:{os.environ['S3_BUCKET']}/tiddlywiki"
+        process = rclone_utils.run_cmd(command)
+        if process.returncode == 0:
+            logger.info("Successfully synced to S3")
+        else:
+            logger.error(f"S3 sync failed! \n{process.stderr}")
+    while True:
+        render_schedule.get_work()
+        logger.info("Running render ...")
         render_static_wiki()
         edited_wiki_path = edit_html_tiddlers()
         shutil.copyfile(edited_wiki_path, "/www/index.html")
         os.remove(edited_wiki_path)
         os.rmdir(os.path.dirname(edited_wiki_path))
+        rclone_sync_s3()
+        logger.info(f"Sleeping for (at least) {SLEEP} seconds ...")
+        time.sleep(SLEEP)
 
 def task_worker():
     create_rclone_config()
-    rclone_sync_s3()
+    publish_static_wiki()
     os.makedirs("/tiddlywiki/files", exist_ok=True)
     while True:
         task, title = task_queue.get()
@@ -137,7 +159,6 @@ def task_worker():
                 )
                 if wait_for_file(f"{tiddler_path}.meta"):
                     pass
-        rclone_sync_s3()
         publish_static_wiki()
         task_queue.task_done()
 
@@ -153,10 +174,10 @@ class MyUDPHander(socketserver.BaseRequestHandler):
                 and task in ('save','delete')):
                 task_queue.put((task,title))
             else:
-                rclone_sync_s3()
                 publish_static_wiki()
 def main():
     threading.Thread(target=task_worker, daemon=True).start()
+    threading.Thread(target=render_worker, daemon=True).start()
     HOST, PORT = "0.0.0.0", 2000
     with socketserver.UDPServer((HOST, PORT), MyUDPHander) as server:
         logger.info("Starting socket server on {}:{}".format(HOST, PORT))
