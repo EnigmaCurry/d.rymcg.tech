8c5a83a0dd44773b174b3620f33a75a7d2a5ff11
EnigmaCurry <ryan@enigmacurry.com>
2023-01-31 16:42:48 -0800

    templates

--- _templates/python-flask/flask/api/__init__.py
+++ _templates/python-flask/flask/api/__init__.py
@@ -13,10 +13,9 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 def hello_world():
     return f"""
     <h1>Hello, World!</h1>
-    <p>Project: {os.environ.get('PROJECT')}</p>
-    <p>Instance: {os.environ.get('INSTANCE')}</p>
-    <p>Docker Context: {os.environ.get('CONTEXT')}</p>
-    <p>Traefik Host route: {os.environ['TRAEFIK_HOST']}</p>
+    <p>Project: {os.environ.get('DOCKER_PROJECT')}</p>
+    <p>Instance: {os.environ.get('DOCKER_INSTANCE')}</p>
+    <p>Docker Context: {os.environ.get('DOCKER_CONTEXT')}</p>
     <p>Your IP address is: {request.headers['X-Forwarded-For']}</p>
     <p>Your User Agent: {request.headers['User-Agent']}</p>
     """
