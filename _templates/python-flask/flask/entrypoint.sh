575c7e68c7932b144922b25e83b2384dc18c9eba
EnigmaCurry <ryan@enigmacurry.com>
2023-02-01 00:05:56 -0800

    python-flask

--- _templates/python-flask/flask/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Starting server with public traefik host: https://${TRAEFIK_HOST}"
-set -ex
-gunicorn --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread --bind 0.0.0.0 api:app "$@"
