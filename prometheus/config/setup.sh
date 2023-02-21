7a7d776e03b2cb6bf49adc11ad67503e1a8ac5f4
Ryan McGuire <ryan@enigmacurry.com>
2023-02-21 14:51:36 -0500

    untested alertmanager

--- prometheus/config/setup.sh
+++ prometheus/config/setup.sh
@@ -7,6 +7,11 @@ ytt_template() {
     ytt -f ${src} \
         -v node_exporter_enabled=${PROMETHEUS_NODE_EXPORTER_ENABLED} \
         -v cadvisor_enabled=${PROMETHEUS_CADVISOR_ENABLED} \
+        -v alertmanager_enabled=${PROMETHEUS_ALERTMANAGER_ENABLED} \
+        -v smtp_smarthost=${PROMETHEUS_ALERTMANAGER_SMTP_SMARTHOST} \
+        -v smtp_auth_username=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_USERNAME} \
+        -v smtp_auth_password=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_PASSWORD} \
+        -v smtp_from=${PROMETHEUS_ALERTMANAGER_SMTP_FROM} \
         > ${dst}
     success=$?
     echo "[ ! ] GENERATED NEW CONFIG FILE :::  ${dst}"
@@ -17,6 +22,10 @@ create_config() {
     rm -rf /etc/prometheus/*
     ytt_template prometheus.yml /etc/prometheus/prometheus.yml
     cat /etc/prometheus/prometheus.yml
+
+    rm -rf /etc/alertmanager/*
+    ytt_template alertmanager.yml /etc/alertmanager/alertmanager.yml
+    cat /etc/alertmanager/alertmanager.yml
 }
 
 create_config
