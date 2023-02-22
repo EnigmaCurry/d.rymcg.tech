dd21ba13296f56c0e6397833fb62c484e4bd077a
Ryan McGuire <ryan@enigmacurry.com>
2023-02-21 23:35:44 -0500

    fix alertmanager smtp config

--- prometheus/config/setup.sh
+++ prometheus/config/setup.sh
@@ -8,10 +8,12 @@ ytt_template() {
         -v node_exporter_enabled=${PROMETHEUS_NODE_EXPORTER_ENABLED} \
         -v cadvisor_enabled=${PROMETHEUS_CADVISOR_ENABLED} \
         -v alertmanager_enabled=${PROMETHEUS_ALERTMANAGER_ENABLED} \
+        -v smtp_enabled=${PROMETHEUS_ALERTMANAGER_SMTP_ENABLED} \
         -v smtp_smarthost=${PROMETHEUS_ALERTMANAGER_SMTP_SMARTHOST} \
         -v smtp_auth_username=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_USERNAME} \
         -v smtp_auth_password=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_PASSWORD} \
         -v smtp_from=${PROMETHEUS_ALERTMANAGER_SMTP_FROM} \
+        -v smtp_to=${PROMETHEUS_ALERTMANAGER_SMTP_TO} \
         > ${dst}
     success=$?
     echo "[ ! ] GENERATED NEW CONFIG FILE :::  ${dst}"
@@ -21,11 +23,11 @@ ytt_template() {
 create_config() {
     rm -rf /etc/prometheus/*
     ytt_template prometheus.yml /etc/prometheus/prometheus.yml
-    cat /etc/prometheus/prometheus.yml
+    #cat /etc/prometheus/prometheus.yml
 
     rm -rf /etc/alertmanager/*
     ytt_template alertmanager.yml /etc/alertmanager/alertmanager.yml
-    cat /etc/alertmanager/alertmanager.yml
+    #cat /etc/alertmanager/alertmanager.yml
 }
 
 create_config
