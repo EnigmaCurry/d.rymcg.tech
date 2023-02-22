63872f32f6b0f2b9f01d50a499331a6bd58d2089
Ryan McGuire <ryan@enigmacurry.com>
2023-02-22 03:20:06 -0500

    Prometheus (#39)
    * prometheus
    
    * README
    
    * profiles for cadvisor and node-exporter
    
    * cadvisor tuning
    
    * README
    
    * untested alertmanager
    
    * Fix alertmanager config
    
    * fix alertmanager smtp config
    
    * memory limits
    
    * alerts test

---
+++ prometheus/config/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+ytt_template() {
+    src=$1; dst=$2;
+    [ -e "${src}" ] || (echo "Template not found: ${src}" && exit 1)
+    ytt -f ${src} \
+        -v node_exporter_enabled=${PROMETHEUS_NODE_EXPORTER_ENABLED} \
+        -v cadvisor_enabled=${PROMETHEUS_CADVISOR_ENABLED} \
+        -v alertmanager_enabled=${PROMETHEUS_ALERTMANAGER_ENABLED} \
+        -v smtp_enabled=${PROMETHEUS_ALERTMANAGER_SMTP_ENABLED} \
+        -v smtp_smarthost=${PROMETHEUS_ALERTMANAGER_SMTP_SMARTHOST} \
+        -v smtp_auth_username=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_USERNAME} \
+        -v smtp_auth_password=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_PASSWORD} \
+        -v smtp_from=${PROMETHEUS_ALERTMANAGER_SMTP_FROM} \
+        -v smtp_to=${PROMETHEUS_ALERTMANAGER_SMTP_TO} \
+        > ${dst}
+    success=$?
+    echo "[ ! ] GENERATED NEW CONFIG FILE :::  ${dst}"
+    return ${success}
+}
+
+create_config() {
+    rm -rf /etc/prometheus/*
+    ytt_template prometheus.yml /etc/prometheus/prometheus.yml
+    #cat /etc/prometheus/prometheus.yml
+
+    rm -rf /etc/alertmanager/*
+    ytt_template alertmanager.yml /etc/alertmanager/alertmanager.yml
+    #cat /etc/alertmanager/alertmanager.yml
+}
+
+create_config
