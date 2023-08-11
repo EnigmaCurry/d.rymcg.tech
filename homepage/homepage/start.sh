d850f3c0736e0f9d74cdbc84fbeb679a10c5a990
mcmikemn <mike@thewooskeys.com>
2023-08-11 13:08:15 -0400

    updated comments

--- homepage/homepage/start.sh
+++ homepage/homepage/start.sh
@@ -12,7 +12,9 @@ if ! grep github.com "${SSH_KNOWNHOSTS_FILE}" > /dev/null
 then
      echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=" >> "${SSH_KNOWNHOSTS_FILE}"
 fi
-## Pull domain from HOMEPAGE_TEMPLATE_REPO.
+## Pull domain from HOMEPAGE_TEMPLATE_REPO, which can be in 1 of 2 formats:
+## it@github.com:YourUsername/my-private-homepage-template.git
+## https://github.com/EnigmaCurry/d.rymcg.tech_homepage-template.git
 REPO_DOMAIN=$(echo "${HOMEPAGE_TEMPLATE_REPO}" | grep -oP '(?<=@|:\/\/)([^\/:]+)' | sed 's/[^@]*@//')
 ## Pull port from HOMEPAGE_TEMPLATE_REPO.
 ## I'm not setting the default port of 22 if no port is designated in
