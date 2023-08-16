c6b0a82ad1fd7539240422a491f1bab837481278
EnigmaCurry <ryan@enigmacurry.com>
2023-08-15 22:19:07 -0600

    docker workstation

--- _terminal/docker-workstation/src/sshd_entrypoint.sh
+++ _terminal/docker-workstation/src/sshd_entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir -p /etc/ssh/keys
+echo "----------------------------------------"
+echo "## Startup at $(date)"
+mkdir -p /home/ssh/keys
 for key_type in ed25519 rsa; do
-    if [[ ! -f /etc/ssh/keys/ssh_host_${key_type}_key ]]; then
+    if [[ ! -f /home/ssh/keys/ssh_host_${key_type}_key ]]; then
         echo "Generating new SSH host key type: ${key_type}"
-        ssh-keygen -N "" -t "${key_type}" -f "/etc/ssh/keys/ssh_host_${key_type}_key"
+        ssh-keygen -N "" -t "${key_type}" -f "/home/ssh/keys/ssh_host_${key_type}_key"
     fi
 done
 
