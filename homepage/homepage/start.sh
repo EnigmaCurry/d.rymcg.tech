323f935e1fcafa2a288d5b05230b421d00d1afda
mcmikemn <mike@thewooskeys.com>
2023-08-23 15:11:20 -0400

    exclude Dockerfile from git clone in start.sh

--- homepage/homepage/start.sh
+++ homepage/homepage/start.sh
@@ -48,6 +48,8 @@ if [[ -n "${HOMEPAGE_TEMPLATE_REPO}" ]]; then
                 cat "${file}" | envsubst > $out_path
                 echo "Rendered template file: $out_path"
             done
+            mv "${TMP_CLONE}/!(*.yaml|Dockerfile)" /app/config/
+            
             rm -rf ${TMP_CLONE}
         else
             echo "ERROR: Could not clone from git repository: ${HOMEPAGE_TEMPLATE_REPO}"
