a5b7a68b90c15cce6cb0032b37594200b4462657
EnigmaCurry <ryan@enigmacurry.com>
2023-10-28 16:11:08 -0600

    backup works

--- postgresql/backup.sh
+++ postgresql/backup.sh
@@ -5,8 +5,8 @@ source ${BIN}/funcs.sh
 
 check_var ENV_FILE
 echo ENV_FILE=${ENV_FILE}
-echo INSTANCE=${INSTANCE}
-echo INSTANCE_SUFFIX=${INSTANCE_SUFFIX}
+echo INSTANCE=${INSTANCE:-default}
+echo INSTANCE_SUFFIX=${INSTANCE_SUFFIX:-_default}
 export PROJECT_NAME
 export INSTANCE
 export INSTANCE_SUFFIX
@@ -17,8 +17,9 @@ pgbackrest_cmd() {
     if [[ "backup" == "$1" ]]; then
         EXTRA_ARGS+=("--annotation=context=${DOCKER_CONTEXT}" "--annotation=instance=${INSTANCE}")
     fi
+    EXTRA_ARGS+=($@)
     EXTRA_ARGS="${EXTRA_ARGS[@]}"
-    docker_compose exec -u postgres postgres sh -c "set -x; pgbackrest ${EXTRA_ARGS} $@"
+    docker_compose exec -u postgres postgres sh -c "set -x; pgbackrest ${EXTRA_ARGS}"
 }
 
 wait_for_ready() {
@@ -30,11 +31,26 @@ restart() {
     wait_for_ready
 }
 
+backup_local() {
+    if [[ "$(${BIN}/dotenv -f ${ENV_FILE} get POSTGRES_PGBACKREST_LOCAL)"  == "true" ]]; then
+        pgbackrest_cmd backup --repo=1
+    else
+        echo "## Skipping local backup as POSTGRES_PGBACKREST_LOCAL != true"
+    fi
+}
+
+backup_s3() {
+    if [[ "$(${BIN}/dotenv -f ${ENV_FILE} get POSTGRES_PGBACKREST_S3)"  == "true" ]]; then
+        pgbackrest_cmd backup --repo=2
+    else
+        echo "## Skipping local backup as POSTGRES_PGBACKREST_S3 != true"
+    fi
+}
+
 set -e
 wait_for_ready
 pgbackrest_cmd stanza-create
-pgbackrest_cmd check
-pgbackrest_cmd backup
+backup_local
+backup_s3
 pgbackrest_cmd info
-
-#pgbackrest_cmd restore
+pgbackrest_cmd check
