a502e0db2149c44a8f26db416a4dbf886fb3fd57
EnigmaCurry <ryan@enigmacurry.com>
2023-12-30 14:47:16 -0700

    traefik-forward-auth provider selection

---
+++ traefik-forward-auth/configure_gitea.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BIN=${ROOT_DIR}/_scripts
+source ${BIN}/funcs.sh
+
+ROOT_DOMAIN=$(get_root_domain)
+DOCKER_CONTEXT=$(${BIN}/docker_context)
+
+${BIN}/reconfigure_ask ${ENV_FILE} TRAEFIK_FORWARD_AUTH_GITEA_DOMAIN "Enter your gitea domain name" git.${ROOT_DOMAIN}
+
+echo ""
+echo "Opening Gitea applications page... (login as root)"
+echo "You should now create a new OAuth2 application: "
+echo "Set the 'Application Name' the same as AUTH_HOST (or whatever you like)"
+echo "Set the 'Redirect URL' using https://AUTH_HOST/_oauth, eg. https://auth.${ROOT_DOMAIN}/_oauth"
+
+GITEA_DOMAIN=$(${BIN}/dotenv -f ${ENV_FILE} get TRAEFIK_FORWARD_AUTH_GITEA_DOMAIN)
+HTTPS_PORT=$(${BIN}/dotenv -f ${ENV_FILE} get TRAEFIK_FORWARD_AUTH_HTTPS_PORT)
+
+xdg-open https://${GITEA_DOMAIN}${HTTPS_PORT}/user/settings/applications
+
+${BIN}/reconfigure_ask ${ENV_FILE} TRAEFIK_FORWARD_AUTH_PROVIDERS_GENERIC_OAUTH_CLIENT_ID "Copy and Paste the OAuth2 client ID here"
+
+${BIN}/reconfigure_ask ${ENV_FILE} TRAEFIK_FORWARD_AUTH_PROVIDERS_GENERIC_OAUTH_CLIENT_SECRET "Copy and Paste the OAuth2 client secret here"
+${BIN}/reconfigure_ask ${ENV_FILE} TRAEFIK_FORWARD_AUTH_LOGOUT_REDIRECT "Enter the logout redirect URL" https://${GITEA_DOMAIN}$(${BIN}/dotenv -f ${ENV_FILE} get TRAEFIK_FORWARD_AUTH_HTTPS_PORT)/logout
+
+${BIN}/reconfigure ${ENV_FILE} \
+      TRAEFIK_FORWARD_AUTH_SECRET="$(shell openssl rand -base64 45)" \
+      TRAEFIK_FORWARD_AUTH_PROVIDERS_GENERIC_OAUTH_AUTH_URL="https://${GITEA_DOMAIN}${HTTPS_PORT}/login/oauth/authorize" \
+      TRAEFIK_FORWARD_AUTH_PROVIDERS_GENERIC_OAUTH_TOKEN_URL="https://${GITEA_DOMAIN}${HTTPS_PORT}/login/oauth/access_token" \
+      TRAEFIK_FORWARD_AUTH_PROVIDERS_GENERIC_OAUTH_USER_URL="https://${GITEA_DOMAIN}${HTTPS_PORT}/api/v1/user"
---
+++ traefik-forward-auth/configure_google.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BIN=${ROOT_DIR}/_scripts
+source ${BIN}/funcs.sh
+
+ROOT_DOMAIN=$(get_root_domain)
+DOCKER_CONTEXT=$(${BIN}/docker_context)
+
+fault TODO
