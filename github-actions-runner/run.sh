07aad7ba05588c9d24aeb28444e0222767a5aee9
EnigmaCurry <ryan@enigmacurry.com>
2024-07-04 10:58:11 -0600

    Fix github-actions-runner

--- github-actions-runner/run.sh
+++ github-actions-runner/run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker context create --docker "host=tcp://docker:2376,ca=/certs/client/ca.pem,cert=/certs/client/cert.pem,key=/certs/client/key.pem" docker
+docker context create --docker "host=tcp://docker:2376,ca=/certs/client/ca.pem,cert=/certs/client/cert.pem,key=/certs/client/key.pem" docker || true
 docker context use docker
 
 test -f .credentials || /actions-runner/config.sh  --unattended --url ${REPOSITORY} --token ${RUNNER_TOKEN}
