4cefdfce260804e2839fe92fdf314db99553b66d
EnigmaCurry <ryan@enigmacurry.com>
2024-07-04 13:11:39 -0600

    fix github-actions-runner

--- github-actions-runner/run.sh
+++ github-actions-runner/run.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+PATH=${HOME}/.cargo/bin:${PATH}
+export CROSS_CONTAINER_IN_CONTAINER=true
 docker context create --docker "host=tcp://docker:2376,ca=/certs/client/ca.pem,cert=/certs/client/cert.pem,key=/certs/client/key.pem" docker || true
 docker context use docker
 
