fa05431a2710ea8d935da58bc7a26685dc6fe1ac
EnigmaCurry <ryan@enigmacurry.com>
2024-10-18 15:34:04 -0600

    vars

--- _scripts/bootstrap_sworkstation.sh
+++ _scripts/bootstrap_sworkstation.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 ## Quick setup of a d.rymcg.tech Sworkstation in a fresh debian VM:
-## ROOT_DOMAIN=d.example.com bash <(curl https://raw.githubusercontent.com/EnigmaCurry/d.rymcg.tech/refs/heads/master/_scripts/bootstrap_sworkstation.sh)
+## CONTEXT=l ROOT_DOMAIN=d.example.com bash <(curl https://raw.githubusercontent.com/EnigmaCurry/d.rymcg.tech/refs/heads/master/_scripts/bootstrap_sworkstation.sh)
 (set -ex
 if [ ! -f /etc/debian_version ]; then
     echo "This script should only be run on Debian-based systems."
     exit 1
 fi
+HOST="${HOST:-localhost}"
+CONTEXT="${CONTEXT:-${HOST}}"
+ROOT_DOMAIN="${ROOT_DOMAIN:-${HOST}}"
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install \
     --assume-yes \
@@ -17,35 +20,39 @@ curl -sSL https://get.docker.com | sh
 ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub | sudo tee -a /root/.ssh/authorized_keys
 cat <<EOF >> ~/.ssh/config
-Host localhost
+Host ${CONTEXT}
     User root
+    Hostname ${HOST}
     ControlMaster auto
     ControlPersist yes
     ControlPath /tmp/ssh-%u-%r@%h:%p
 EOF
 sudo systemctl enable --now ssh
 ssh-keyscan -H localhost >> ~/.ssh/known_hosts
-ssh localhost whoami
-docker context create localhost --docker "host=ssh://localhost"
-docker context use localhost
+ssh "${HOST}" whoami
+docker context create "${CONTEXT}" --docker "host=ssh://${HOST}"
+docker context use "${CONTEXT}"
 git clone https://github.com/EnigmaCurry/d.rymcg.tech.git \
     ${HOME}/git/vendor/enigmacurry/d.rymcg.tech
 cat <<'EOF' >> ~/.bashrc
+
 export EDITOR=nano
 ## d.rymcg.tech cli tool:
 export PATH=${PATH}:${HOME}/git/vendor/enigmacurry/d.rymcg.tech/_scripts/user
 eval "$(d.rymcg.tech completion bash)"
 __d.rymcg.tech_cli_alias d
-## Add d.rymcg.tech alias for each Docker context:
-__d.rymcg.tech_context_alias localhost
 ## Fix for the root user to always use default context:
 if [ "$(whoami)" = "root" ]; then
   export DOCKER_CONTEXT=default
 fi
 EOF
-shopt -s expand_aliases
+cat <<EOF >> ~/.bashrc
+## Add d.rymcg.tech alias for each Docker context:
+__d.rymcg.tech_context_alias ${CONTEXT}
+
+EOF
 source ~/.bashrc
-ROOT_DOMAIN=${ROOT_DOMAIN:-localhost} USE_ENV=true YES=yes \
+ROOT_DOMAIN=${ROOT_DOMAIN} USE_ENV=true YES=yes \
 d.rymcg.tech tmp-context localhost d.rymcg.tech config
 set +x
 echo
