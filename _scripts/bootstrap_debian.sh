2e8c2eb33ca3b63dfedc220fc63c7fcc198b7b3d
EnigmaCurry <ryan@enigmacurry.com>
2024-10-18 15:17:10 -0600

    bootstrap debian

---
+++ _scripts/bootstrap_debian.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+## Quick setup of a d.rymcg.tech Sworkstation in a fresh debian VM:
+## ROOT_DOMAIN=d.example.com bash <(curl https://gist.githubusercontent.com/EnigmaCurry/c6fc7afe756b1a2741ee869c870fc845/raw)
+(set -ex
+if [ ! -f /etc/debian_version ]; then
+    echo "This script should only be run on Debian-based systems."
+    exit 1
+fi
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install \
+    --assume-yes \
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold" \
+    bash build-essential gettext git openssl apache2-utils xdg-utils jq sshfs wireguard \
+    curl inotify-tools w3m nano openssh-server
+curl -sSL https://get.docker.com | sh 
+ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub | sudo tee -a /root/.ssh/authorized_keys
+cat <<EOF >> ~/.ssh/config
+Host localhost
+    User root
+    ControlMaster auto
+    ControlPersist yes
+    ControlPath /tmp/ssh-%u-%r@%h:%p
+EOF
+sudo systemctl enable --now ssh
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts
+ssh localhost whoami
+docker context create localhost --docker "host=ssh://localhost"
+docker context use localhost
+git clone https://github.com/EnigmaCurry/d.rymcg.tech.git \
+    ${HOME}/git/vendor/enigmacurry/d.rymcg.tech
+cat <<'EOF' >> ~/.bashrc
+export EDITOR=nano
+## d.rymcg.tech cli tool:
+export PATH=${PATH}:${HOME}/git/vendor/enigmacurry/d.rymcg.tech/_scripts/user
+eval "$(d.rymcg.tech completion bash)"
+__d.rymcg.tech_cli_alias d
+## Add d.rymcg.tech alias for each Docker context:
+__d.rymcg.tech_context_alias localhost
+## Fix for the root user to always use default context:
+if [ "$(whoami)" = "root" ]; then
+  export DOCKER_CONTEXT=default
+fi
+EOF
+shopt -s expand_aliases
+source ~/.bashrc
+ROOT_DOMAIN=${ROOT_DOMAIN:-localhost} USE_ENV=true YES=yes \
+d.rymcg.tech tmp-context localhost d.rymcg.tech config
+set +x
+echo
+echo
+echo "## Installation finished."
+echo "## Log out and log back in (or source ~/.bashrc)"
+echo "## Use the 'localhost' alias to manage the local Docker host."
+)
