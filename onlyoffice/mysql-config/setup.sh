d2eb3bcb0386d6631d7a9c8c11d88fdfd2dd8907
Mike Wooskey <mike@thewooskeys.com>
2024-11-08 17:44:52 +0000

    still working: envsubst isn't working in Dockerfile

--- onlyoffice/mysql-config/setup.sh
+++ onlyoffice/mysql-config/setup.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 set -e
+set -x # debug
 
-ytt_template() {
+gettext_template() {
     src=$1; dst=$2;
+    dst_path=$(dirname "$2")
+    
+    mkdir -p ${dst_path}
+    rm -rf ${dst_path}/*
+
     [ -e "${src}" ] || (echo "Template not found: ${src}" && exit 1)
-    ytt -f ${src} \
-        -v node_exporter_enabled=${PROMETHEUS_NODE_EXPORTER_ENABLED} \
-        -v cadvisor_enabled=${PROMETHEUS_CADVISOR_ENABLED} \
-        -v alertmanager_enabled=${PROMETHEUS_ALERTMANAGER_ENABLED} \
-        -v smtp_enabled=${PROMETHEUS_ALERTMANAGER_SMTP_ENABLED} \
-        -v smtp_smarthost=${PROMETHEUS_ALERTMANAGER_SMTP_SMARTHOST} \
-        -v smtp_auth_username=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_USERNAME} \
-        -v smtp_auth_password=${PROMETHEUS_ALERTMANAGER_SMTP_AUTH_PASSWORD} \
-        -v smtp_from=${PROMETHEUS_ALERTMANAGER_SMTP_FROM} \
-        -v smtp_to=${PROMETHEUS_ALERTMANAGER_SMTP_TO} \
-        > ${dst}
+    envsubst < ${src} > ${dst}
     success=$?
     echo "[ ! ] GENERATED NEW CONFIG FILE :::  ${dst}"
     return ${success}
 }
 
 create_config() {
-    rm -rf /etc/mysql/conf.d/*
-    # Currently there are no env vars to template in `onlyoffice.cnf` but we need to copy the file anyway so I'm letting ytt do it. Maybe in the future we'll variablize some of it.
-    ytt_template onlyoffice.cnf /etc/mysql/conf.d/onlyoffice.cnf
-    #cat /etc/mysql/conf.d/onlyoffice.cnf
+    # Currently there are no env vars to template in `onlyoffice.cnf` but we need to copy the file anyway so I'm letting envsubst do it. Maybe in the future we'll variablize some of it.
+    gettext_template onlyoffice.cnf /etc/mysql/conf.d/onlyoffice.cnf
+    cat /etc/mysql/conf.d/onlyoffice.cnf # debug
 
-    rm -rf /docker-entrypoint-initdb.d/*
-    ytt_template setup.sql /docker-entrypoint-initdb.d/setup.sql
-    #cat /docker-entrypoint-initdb.d/setup.sql
+    gettext_template setup.sql /docker-entrypoint-initdb.d/setup.sql
+    cat /docker-entrypoint-initdb.d/setup.sql # debug
 }
 
 create_config
