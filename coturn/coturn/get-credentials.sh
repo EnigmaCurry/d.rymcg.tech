de5b3223041712cff88a2b4895f5e28c5e0aae11
EnigmaCurry <ryan@enigmacurry.com>
2025-09-17 19:57:45 -0600

    coturn updates

--- coturn/coturn/get-credentials.sh
+++ coturn/coturn/get-credentials.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 USERID=test
 
 # Configuration
 : "${STATIC_AUTH_SECRET:?Need to set STATIC_AUTH_SECRET}"
+: "${HOST:?Need to set HOST}"
 : "${REALM:?Need to set REALM}"
 : "${USERID:?Need to set USERID}"   # <-- User ID to embed
 TTL_SECONDS="${TTL_SECONDS:-300}"   # Default 5 minutes TTL
@@ -21,10 +22,28 @@ PASSWORD=$(echo -n "$USERNAME" | openssl dgst -binary -sha1 -hmac "$STATIC_AUTH_
 # Expiration time (human readable)
 EXPIRES_AT=$(date -d "@$(echo $USERNAME | cut -d: -f1)" "+%Y-%m-%d %H:%M:%S %Z")
 
+
+human_duration() {
+  local s=$1 neg= parts=()
+  (( s < 0 )) && { neg="-"; s=$((-s)); }
+  local d=$(( s/86400 )); s=$(( s%86400 ))
+  local h=$(( s/3600 ));  s=$(( s%3600 ))
+  local m=$(( s/60 ));    s=$(( s%60 ))
+  (( d )) && parts+=("$d day$([[ $d != 1 ]] && echo s)")
+  (( h )) && parts+=("$h hour$([[ $h != 1 ]] && echo s)")
+  (( m )) && parts+=("$m minute$([[ $m != 1 ]] && echo s)")
+  (( s )) && parts+=("$s second$([[ $s != 1 ]] && echo s)")
+  [[ ${#parts[@]} -eq 0 ]] && parts=("0 seconds")
+  printf "%s%s" "$neg" "$(IFS=", "; echo "${parts[*]}")"
+}
+
+
 # Output (split labels and values cleanly)
 echo
-echo "TURN URI:"
-echo "turns:${REALM}:3478"
+echo "TURN URIs:"
+echo
+echo "turns:${HOST}:443?transport=tcp"
+echo "turn:${HOST}:3478?transport=udp"
 echo
 echo "Username:"
 echo "$USERNAME"
@@ -33,7 +52,7 @@ echo "Password:"
 echo "$PASSWORD"
 echo
 echo "Expires at:"
-echo "$EXPIRES_AT (in $(($TTL_SECONDS/60)) minutes)"
+echo "$EXPIRES_AT (in $(human_duration "$TTL_SECONDS"))"
 echo
 echo "Test URL:"
 echo "https://webrtc.github.io/samples/src/content/peerconnection/trickle-ice/"
