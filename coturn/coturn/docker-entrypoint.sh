4e4cc559eae1bafcacd1c9e0bd38265f8f84bb7b
Ryan McGuire <ryan@enigmacurry.com>
2025-09-17 23:49:55 -0400

    Coturn updates (#492)
    * coturn updates
    
    * fix auth username
    
    * fix long credential period

--- coturn/coturn/docker-entrypoint.sh
+++ coturn/coturn/docker-entrypoint.sh
@@ -6,34 +6,6 @@ if [ "${1:0:1}" == '-' ]; then
   set -- turnserver "$@"
 fi
 
-# Wait for TLS cert and key
-echo "Waiting for TLS cert and key to be available..."
-
-while [ ! -f "$CERT_PATH" ]; do
-    echo "Waiting for cert file at $CERT_PATH..."
-    sleep 1
-done
-
-while [ ! -f "$KEY_PATH" ]; do
-    echo "Waiting for key file at $KEY_PATH..."
-    sleep 1
-done
-
-echo "Certificates found, copying..."
-
-# Copy certs to internal directory and fix ownership
-mkdir -p /var/lib/coturn/certs
-cp "$CERT_PATH" /var/lib/coturn/certs/fullchain.pem
-cp "$KEY_PATH" /var/lib/coturn/certs/privkey.pem
-
-chown nobody:nogroup /var/lib/coturn/certs/fullchain.pem
-chown nobody:nogroup /var/lib/coturn/certs/privkey.pem
-chmod 600 /var/lib/coturn/certs/*
-
-# Update paths to point to copied certs
-export CERT_PATH=/var/lib/coturn/certs/fullchain.pem
-export KEY_PATH=/var/lib/coturn/certs/privkey.pem
-
 # Drop privileges manually
 echo "Dropping privileges to nobody:nogroup"
 exec gosu nobody:nogroup "$@"
