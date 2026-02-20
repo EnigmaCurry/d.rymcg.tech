f409748e13377ca560ed33e4307583bb7156ffb2
EnigmaCurry <ryan@enigmacurry.com>
2026-02-20 18:44:31 +0000

    Disable Docker daemon on workstation USB

--- nix/workstation/docker.nix
+++ nix/workstation/docker.nix
@@ -2,24 +2,15 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Enable Docker daemon
-  virtualisation.docker.enable = true;
+  # Docker daemon disabled — this workstation uses remote Docker contexts
+  virtualisation.docker.enable = false;
 
   # Enable libvirtd for virt-manager / QEMU / KVM
   virtualisation.libvirtd.enable = true;
 
-  # Trust the Docker bridge so container traffic passes the firewall
-  networking.firewall.trustedInterfaces = [ "docker0" ];
-
-  # Prevent systemd-networkd from managing Docker's veth interfaces
-  systemd.network.networks."10-docker-veth" = {
-    matchConfig.Driver = "veth";
-    linkConfig.Unmanaged = "yes";
-  };
-
-  # Only admin gets local docker and libvirtd access
+  # Only admin gets libvirtd access
   # (user can use remote Docker contexts and user-level QEMU sessions)
-  users.users.admin.extraGroups = [ "docker" "libvirtd" ];
+  users.users.admin.extraGroups = [ "libvirtd" ];
 
   # Traefik UID reservation for Docker containers
   users.users.traefik = {
