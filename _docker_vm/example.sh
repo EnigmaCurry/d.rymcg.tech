a3d82d38a000744e1b92b105d7d9be444c1ce072
EnigmaCurry <ryan@enigmacurry.com>
2026-03-02 10:49:29 -0700

    remove _docker_vm like master did

--- _docker_vm/example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Example to run public webserver, postgresql, and SSH on unprivileged ports:
-export VMNAME="docker-vm"
-export EXTRA_PORTS='8000:80,8443:443,5432:5432,2222:2222'
-export HOSTFWD_HOST='*'
-
-make install
-make enable
