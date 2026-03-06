df7620b991b2368d445ec59fbef2dfd18fbc9ca1
EnigmaCurry <ryan@enigmacurry.com>
2026-03-06 03:53:54 +0000

    Replace password auth with Step-CA mTLS for NATS client connections

--- nats/config/setup.sh
+++ nats/config/setup.sh
@@ -8,12 +8,12 @@ if [[ -z "${NATS_CLUSTER_NAME}" ]]; then
     exit 1
 fi
 
-if [[ -z "${NATS_AUTHORIZATION_PASSWORD}" ]]; then
-    echo "NATS_AUTHORIZATION_PASSWORD is empty."
+if [[ -z "${NATS_TRAEFIK_HOST}" ]]; then
+    echo "NATS_TRAEFIK_HOST is empty."
     exit 1
 fi
 
 echo "Creating new NATS config from template ..."
 mkdir -p ${CONFIG_DIR}
-cat /template/nats.conf | envsubst '${NATS_CLUSTER_NAME},${NATS_AUTHORIZATION_PASSWORD}' > ${CONFIG}
+cat /template/nats.conf | envsubst '${NATS_CLUSTER_NAME},${NATS_TRAEFIK_HOST}' > ${CONFIG}
 echo "[ ! ] GENERATED NEW CONFIG FILE ::: ${CONFIG}"
