778ec04667f91a2e826db843aaf27164e8ef9feb
EnigmaCurry <ryan@enigmacurry.com>
2026-03-15 04:10:05 +0000

    Fix create-ssh-role to pipe JSON via stdin for map fields

--- openbao/create-ssh-role.sh
+++ openbao/create-ssh-role.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 ## Creates the SSH signing role for short-lived certificates.
 ## This script runs inside the openbao container.
-## Usage: bao write ... uses stdin for default_extensions JSON.
 set -e
 
-bao write ssh-client-signer/roles/woodpecker-short-lived \
-    allow_user_certificates=true \
-    allowed_users="*" \
-    key_type=ca \
-    default_user=root \
-    ttl=2m \
-    max_ttl=5m \
-    algorithm_signer=default \
-    default_extensions='{"permit-pty":"","permit-port-forwarding":""}'
+cat <<'EOF' | bao write ssh-client-signer/roles/woodpecker-short-lived -
+{
+  "allow_user_certificates": true,
+  "allowed_users": "*",
+  "default_extensions": {
+    "permit-pty": "",
+    "permit-port-forwarding": ""
+  },
+  "key_type": "ca",
+  "default_user": "root",
+  "ttl": "2m",
+  "max_ttl": "5m",
+  "algorithm_signer": "default"
+}
+EOF
