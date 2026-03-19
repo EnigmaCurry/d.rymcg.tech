038670d1fa3e8d89ee0166890bab0df65f6073d5
EnigmaCurry <ryan@enigmacurry.com>
2026-03-19 06:00:26 +0000

    Add configurable transport timeouts to websecure entrypoint

--- traefik/config/setup.sh
+++ traefik/config/setup.sh
@@ -44,6 +44,9 @@ ytt_template() {
         -v websecure_entrypoint_host="${TRAEFIK_WEBSECURE_ENTRYPOINT_HOST}" \
         -v websecure_entrypoint_port="${TRAEFIK_WEBSECURE_ENTRYPOINT_PORT}" \
         -v websecure_entrypoint_proxy_protocol_trusted_ips="${TRAEFIK_WEBSECURE_ENTRYPOINT_PROXY_PROTOCOL_TRUSTED_IPS}" \
+        -v websecure_read_timeout="${TRAEFIK_WEBSECURE_READ_TIMEOUT}" \
+        -v websecure_write_timeout="${TRAEFIK_WEBSECURE_WRITE_TIMEOUT}" \
+        -v websecure_idle_timeout="${TRAEFIK_WEBSECURE_IDLE_TIMEOUT}" \
         -v web_plain_entrypoint_enabled="${TRAEFIK_WEB_PLAIN_ENTRYPOINT_ENABLED}" \
         -v web_plain_entrypoint_host="${TRAEFIK_WEB_PLAIN_ENTRYPOINT_HOST}" \
         -v web_plain_entrypoint_port="${TRAEFIK_WEB_PLAIN_ENTRYPOINT_PORT}" \
